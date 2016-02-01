ba3b14d838011b1a106ef8f990f20a29dfda52ea
Ed Glazenski <edglazenski@gmail.com>
2016-01-31 20:35:56 -0500

    Added delete todos route

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.post('/todos', function(req, res) {
     body.description = body.description.trim();
 
     body.id = todoNextId;
-    
+
     todos.push(body);
 
 
@@ -46,6 +46,22 @@ app.post('/todos', function(req, res) {
 
     res.json(body);
 
+});
+
+//DELETE /todos/:id
+app.delete('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    if(!matchedTodo) {
+      res.status(404).send();
+    } 
+    else {
+      todos = _.without(todos, matchedTodo);
+      res.json(matchedTodo);
+    }
+
+    
 });
 
 app.listen(port, function() {
