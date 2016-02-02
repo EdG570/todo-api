6fa0c1da39b1089a97aa8f457ad3e62c9a720477
Ed Glazenski <edglazenski@gmail.com>
2016-02-02 16:55:16 -0500

    Final static todos

--- server.js
+++ server.js
@@ -26,6 +26,12 @@ app.get('/todos', function(req, res) {
   else if(filteredTodos.length < 1) {
     res.status(404).send("You don't currently have any todos. Get started by creating one! :)");
   }
+
+  if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+    filteredTodos = _.filter(filteredTodos, function(todo) {
+      return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+    });
+  }
   
   res.json(filteredTodos);
 
