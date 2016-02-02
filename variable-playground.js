7c14e3f37d6d4222b5b402b3f7642fc169cf8347
Ed Glazenski <edglazenski@gmail.com>
2016-02-02 16:07:20 -0500

    Added filters for completed and incomplete todo items

---
+++ variable-playground.js
@@ -0,0 +1,23 @@
+// var person = {
+//     name: 'Andrew',
+//     age: 24
+// };
+
+// function updatePerson(obj) {
+//   obj.age = 37;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+
+var grades = [ 95, 72];
+
+function updateGrades(arr, grade) {
+  arr.push(grade);
+  debugger;
+}
+
+updateGrades(grades, 100);
+console.log(grades);
